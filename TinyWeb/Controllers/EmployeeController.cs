45386fe10da00a7bac71ea4ec1bc75ac99212e8a
74944anand <anandkole9@gmail.com>
2023-12-16 01:21:35 +0530

    TinyWeb added for DotNet with working CRUD operation

---
+++ TinyWeb/Controllers/EmployeeController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TinyWeb.Models;
+
+
+
+namespace TinyWeb.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: EmployeeController
+        public ActionResult Index()
+        {
+            List<Employee> emp = new List<Employee>();
+            emp = Employee.GetEmployees();
+            return View(emp);
+        }
+
+        // GET: EmployeeController/Details/5
+        public ActionResult Details(int id)
+        {
+            Employee emp = Employee.GetEmployee(id);
+            return View(emp);
+        }
+
+        // GET: EmployeeController/Create
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        // POST: EmployeeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Employee emp)
+        {
+            try
+            {
+                Employee.Insert(emp);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Employee e=Employee.GetEmployee(id);
+            return View(e);
+        }
+
+        // POST: EmployeeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id,Employee e)
+        {
+            try
+            {
+                Employee.UpdateEmployee(e);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Employee e = Employee.GetEmployee(id);
+            return View(e);
+        }
+
+        // POST: EmployeeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Employee emp)
+        {
+            try
+            {
+                Employee.DeleteEmployee(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ TinyWeb/Models/Employee.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.Data.SqlClient;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+
+namespace TinyWeb.Models
+{
+    public class Employee
+    {
+        public int EmpNo { get; set; }
+        public string Name { get; set; }
+        public decimal Basic { get; set; }
+        public int DeptNo { get; set; }
+
+
+
+
+
+        public static List<Employee> GetEmployees() {
+            List<Employee> list = new List<Employee>();
+
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Acts2023;Integrated Security=True";
+            try
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "select * from Employee";
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    list.Add(new Employee { EmpNo = dr.GetInt32(0), Name = dr.GetString(1), Basic = dr.GetDecimal(2), DeptNo = dr.GetInt32(3) });
+                }
+                Console.WriteLine("Success");
+                dr.Close();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally { cn.Close(); }
+            return list;
+        }
+
+        public static Employee GetEmployee(int empNo)
+        {
+            Employee emp = new Employee();
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Acts2023;Integrated Security=True";
+            
+                cn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "select * from Employee where EmpNo=@empNo";
+            cmd.Parameters.AddWithValue("EmpNo", empNo);
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    emp.EmpNo = dr.GetInt32(0);
+                    emp.Name = dr.GetString(1);
+                    emp.Basic = dr.GetDecimal(2);
+                    emp.DeptNo = dr.GetInt32(3);
+                }
+                else
+                {
+                    emp = null;
+                }
+                Console.WriteLine("Success");
+                dr.Close();
+                cn.Close();
+                return emp;
+
+            }
+
+        public static void Insert(Employee emp)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Acts2023;Integrated Security=True";
+
+            try
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "insert into Employee values(@EmpNo, @Name, @Basic, @DeptNo)";
+
+                cmd.Parameters.AddWithValue("EmpNo", emp.EmpNo);
+                cmd.Parameters.AddWithValue("Name", emp.Name);
+                cmd.Parameters.AddWithValue("Basic", emp.Basic);
+                cmd.Parameters.AddWithValue("DeptNo", emp.DeptNo);
+                cmd.ExecuteNonQuery();
+
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+            finally { cn.Close(); }
+        }
+
+
+        public static void DeleteEmployee(int id)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Acts2023;Integrated Security=True";
+
+            try
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "delete from Employee where EmpNo=@EmpNo";
+
+                cmd.Parameters.AddWithValue("EmpNo",id);
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally { cn.Close(); }
+        }
+
+        public static void UpdateEmployee(Employee emp)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Acts2023;Integrated Security=True";
+
+            try
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "update Employee set EmpNo=@EmpNo, Name=@Name, Basic=@Basic, DeptNo=@DeptNo  where EmpNo=@EmpNo";
+
+                cmd.Parameters.AddWithValue("EmpNo", emp.EmpNo);
+                cmd.Parameters.AddWithValue("Name", emp.Name);
+                cmd.Parameters.AddWithValue("Basic", emp.Basic);
+                cmd.Parameters.AddWithValue("DeptNo", emp.DeptNo);
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally { cn.Close(); }
+        }
+
+    } } 
+
