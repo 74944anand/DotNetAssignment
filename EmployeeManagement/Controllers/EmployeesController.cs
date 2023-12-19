5d3b52cfd72212993065eec3cb8bd8fabe2a12d7
74944anand <anandkole9@gmail.com>
2023-12-20 03:28:41 +0530

    Employee Management Question solved

---
+++ EmployeeManagement/Controllers/EmployeesController.cs
@@ -0,0 +1,91 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+namespace EmployeeManagement.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: EmployeesController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: EmployeesController/Details/5
+        public ActionResult ViewAllEmployee()
+        {
+            List<Employee> list = new List<Employee>();
+            list=Employee.GetAllEmployees();
+            return View(list);
+        }
+
+        // GET: EmployeesController/Create
+        public ActionResult AddNewEmployee()
+        {
+            return View();
+        }
+
+        // POST: EmployeesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddNewEmployee(Employee e)
+        {
+            try
+            {
+                Employee.AddEmployee(e);
+
+                return RedirectToAction(nameof(ViewAllEmployee));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Employee e = Employee.GetEmployee(id);
+            return View(e);
+        }
+
+        // POST: EmployeesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Employee e)
+        {
+            try
+            {
+                Employee.SetEmployee(e, id);
+                return RedirectToAction(nameof(ViewAllEmployee));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Employee e=Employee.GetEmployee(id);
+            return View(e);
+        }
+
+        // POST: EmployeesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Employee collection)
+        {
+            try
+            {
+                Employee.DeleteEmployee(id);
+                return RedirectToAction(nameof(ViewAllEmployee));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ EmployeeManagement/Models/Employee.cs
@@ -0,0 +1,176 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public string? City { get; set; }
+
+        public string? Address { get; set; }
+
+        public int DeptNo { get; set; } 
+
+
+
+
+        public static List<Employee> GetAllEmployees()
+        {
+            SqlConnection cn = new SqlConnection();
+
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EmpMng;Integrated Security=True;";
+            List<Employee> list = new List<Employee>();
+            try
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "Select * from Employee";
+
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    list.Add(new Employee { Id=dr.GetInt32(0), Name=dr.GetString(1), City=dr.GetString(2), Address=dr.GetString(3), DeptNo=dr.GetInt32(4) });
+                }
+
+
+            }
+            catch(Exception) 
+            {
+
+                throw;
+            }
+            finally { cn.Close(); }
+            return list; 
+
+        }
+
+        public static Employee GetEmployee(int id)
+        {
+            SqlConnection cn = new SqlConnection();
+
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EmpMng;Integrated Security=True;";
+            Employee emp= new Employee();
+            try
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "Select * from Employee where Id=@Id";
+
+                cmd.Parameters.AddWithValue("@Id", id);
+                SqlDataReader dr = cmd.ExecuteReader();
+                
+                while (dr.Read())
+                {
+                    emp.Id = dr.GetInt32(0);
+                    emp.Name = dr.GetString(1);
+                    emp.City = dr.GetString(2);
+                    emp.Address = dr.GetString(3);
+                    emp.DeptNo = dr.GetInt32(4);
+
+                }
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally { cn.Close(); }
+            return emp;
+
+        }
+        public static void AddEmployee(Employee emp)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString= @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EmpMng;Integrated Security=True;";
+            try
+            {
+                cn.Open ();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType= System.Data.CommandType.Text;
+                cmd.CommandText = "insert into Employee values(@Id,@Name,@City,@Address,@DeptNo);";
+
+                cmd.Parameters.AddWithValue("@Id", emp.Id);
+                cmd.Parameters.AddWithValue("@Name", emp.Name);
+                cmd.Parameters.AddWithValue("@City", emp.City);
+                cmd.Parameters.AddWithValue("@Address", emp.Address);
+                cmd.Parameters.AddWithValue("@DeptNo", emp.DeptNo);
+
+                cmd.ExecuteNonQuery();
+            }
+            catch(Exception)
+            {
+                throw;
+                    }
+            finally {  cn.Close(); }
+
+        }
+
+
+        public static void SetEmployee(Employee emp,int id)
+        {
+            SqlConnection cn = new SqlConnection();    
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EmpMng;Integrated Security=True;";
+            try
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "Update Employee set Id=@Id,Name=@Name,City=@City,Address=@Address,DeptNo= @DeptNo where Id=@UId";
+
+
+                cmd.Parameters.AddWithValue("@UId", id);
+                cmd.Parameters.AddWithValue("@Id", emp.Id);
+                cmd.Parameters.AddWithValue("@Name", emp.Name);
+                cmd.Parameters.AddWithValue("@City", emp.City);
+                cmd.Parameters.AddWithValue("@Address", emp.Address);
+                cmd.Parameters.AddWithValue("@DeptNo", emp.DeptNo);
+
+                cmd.ExecuteNonQuery();
+
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message); ;
+            }
+            finally { cn.Close(); }
+
+        }
+
+        public static void DeleteEmployee(int id)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EmpMng;Integrated Security=True;";
+            try
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "delete from employee where Id=@UId";
+
+                cmd.Parameters.AddWithValue("@UId", id);
+
+                cmd.ExecuteNonQuery();
+
+                
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally { cn.Close(); }
+
+        }
+    }
+}
